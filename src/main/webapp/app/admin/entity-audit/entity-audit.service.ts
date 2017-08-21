e2e337ebd6cbe5fbfb30806985750f0dc0245f16
mcmac <spengreb@gmail.com>
2017-08-21 14:16:13 +0100

    Added getPrevVersion and code tidy up

--- src/main/webapp/app/admin/entity-audit/entity-audit.service.ts
+++ src/main/webapp/app/admin/entity-audit/entity-audit.service.ts
@@ -6,34 +6,27 @@ import { Http, RequestOptions, URLSearchParams } from '@angular/http';
 @Injectable()
 export class EntityAuditService {
 
-  constructor(private http: Http) {
-
-  }
+  constructor(private http: Http) { }
 
   getAllAudited(): Observable<string[]> {
-    return this.http.get('api/audits/entity/all').map((response) => response.json())
-    // return Observable.of(['Foo', 'Bar']);
+    return this.http.get('api/audits/entity/all').map((response) => response.json());
   }
 
   findByEntity(entity: string, limit: number): Observable<EntityAuditEvent[]> {
     let params: URLSearchParams = new URLSearchParams();
     params.set('entityType', entity);
     params.set('limit', limit.toString());
-    return this.http.get('api/audits/entity/changes', { search: params }).map((response) => response.json())
+    
+    return this.http.get('api/audits/entity/changes', { search: params }).map((response) => response.json());
+  }
 
+  getPrevVersion(qualifiedName: string, entityId: string, commitVersion: number) {
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('qualifiedName', qualifiedName);
+    params.set('entityId', entityId);
+    params.set('commitVersion', entityId.toString());
 
-    // return Observable.of([
-    //   {
-    //     id: '1',
-    //     entityId: '1011',
-    //     entityType: 'Foo',
-    //     action: 'CREATE',
-    //     entityValue: '{}',
-    //     commitVersion: 1,
-    //     modifiedBy: 'admin',
-    //     modifiedDate: new Date()
-    //   },
-    // ]);
+    return this.http.get('api/audits/entity/changes/version/previous', {search: params}).map((response) => response.json());
   }
 
 }
